168c688b560346942d48826cc286be695e30d415
Andy Mina <andy.mina@mongodb.com>
2022-10-07 19:38:55 -0400

    fix: add missing function to namespace

--- src/energy.cpp
+++ src/energy.cpp
@@ -87,31 +87,31 @@ namespace SeamCarving {
 
       return energy_map;
     }
-  }
 
-  Image ComputeHorizontalEnergyMap(const Image &energy_img) {
-    // setup
-    int rows = energy_img.rows;
-    int cols = energy_img.cols;
-    Image energy_map = Image(rows, cols, CV_32S);
+    Image ComputeHorizontalEnergyMap(const Image &energy_img) {
+      // setup
+      int rows = energy_img.rows;
+      int cols = energy_img.cols;
+      Image energy_map = Image(rows, cols, CV_32S);
 
-    // step 1: initial the right col
-    energy_img.col(cols - 1).copyTo(energy_map.col(cols - 1));
+      // step 1: initial the right col
+      energy_img.col(cols - 1).copyTo(energy_map.col(cols - 1));
 
-    // step 2: move to the next col
-    for (int col = cols - 2; col >= 0; col--) {
-      // step 3: loop through all pixels in this col
-      for (int row = 0; row < rows; row++) {
-        // step 4: compute weights of adjacent pixels
-        const int &center = energy_map.at<int>(row, col + 1);
-        const int &left = (row - 1 < 0) ? std::numeric_limits<int>::max() : energy_map.at<int>(col + 1, row - 1);
-        const int &right = (row + 1 >= rows) ? std::numeric_limits<int>::max() : energy_map.at<int>(col + 1, row + 1);
+      // step 2: move to the next col
+      for (int col = cols - 2; col >= 0; col--) {
+        // step 3: loop through all pixels in this col
+        for (int row = 0; row < rows; row++) {
+          // step 4: compute weights of adjacent pixels
+          const int &center = energy_map.at<int>(row, col + 1);
+          const int &left = (row - 1 < 0) ? std::numeric_limits<int>::max() : energy_map.at<int>(col + 1, row - 1);
+          const int &right = (row + 1 >= rows) ? std::numeric_limits<int>::max() : energy_map.at<int>(col + 1, row + 1);
 
-        // step 5: update the current pixel's weight
-        energy_map.at<int>(row, col) = energy_img.at<uchar>(row, col) + std::min({center, left, right});
+          // step 5: update the current pixel's weight
+          energy_map.at<int>(row, col) = energy_img.at<uchar>(row, col) + std::min({center, left, right});
+        }
       }
-    }
 
-    return energy_map;
+      return energy_map;
+    }
   }
 }

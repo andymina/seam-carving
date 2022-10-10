6af5e730262d1d5179c2da8157991a10e3800297
Andy Mina <mina.andy54@gmail.com>
2022-10-10 15:05:29 -0400

    refactor(#15): clean up energy functions (#24)

--- src/energy.cpp
+++ src/energy.cpp
@@ -1,83 +1,94 @@
 /**
  * Andy Mina
- * 
+ *
  * Implementation of the functions defined in energy.h
-*/
+ */
 #include "energy.h"
 
 namespace SeamCarving {
-  namespace Energy {
-    cv::Mat ComputeEnergy(const cv::Mat &img, const std::string &kernel) {
-      // apply gaussian and convert grayscale
-      cv::Mat src;
-      cv::cvtColor(img, src, cv::COLOR_BGR2GRAY);
-      cv::GaussianBlur(src, src, cv::Size(3, 3), 0, 0);
-      
+namespace Energy {
+void ComputeEnergy(cv::InputOutputArray img) {
+	// apply gaussian and convert grayscale
+	cv::GaussianBlur(img, img, cv::Size(3, 3), 0, 0);
+	cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
 
-      // apply kernel
-      cv::Mat x_edges, y_edges;
-      int ksize = (kernel == "scharr") ? -1 : 3;
-      cv::Sobel(src, x_edges, CV_16S, 1, 0, ksize);
-      cv::Sobel(src, y_edges, CV_16S, 0, 1, ksize);
+	/**
+	 * apply kernel. set the depth on the kernel results to be 16-bit signed
+	 * ints to avoid overflow since img.depth() will be CV_8U;
+	 */
+	cv::Mat x_nrg, y_nrg;
+	/** TODO(#23): investigate if Scharr is better */
+	cv::Sobel(img, x_nrg, CV_16S, 1, 0);
+	cv::Sobel(img, y_nrg, CV_16S, 0, 1);
 
-      // convert and merge
-      cv::Mat abs_x, abs_y;
-      cv::convertScaleAbs(x_edges, abs_x);
-      cv::convertScaleAbs(y_edges, abs_y);
-      cv::addWeighted(abs_x, 0.5, abs_y, 0.5, 0, src);
-      return src;
-    }
-
-    cv::Mat ComputeVerticalEnergyMap(const cv::Mat &energy_img) {
-      // setup
-      int rows = energy_img.rows;
-      int cols = energy_img.cols;
-      cv::Mat energy_map = cv::Mat(rows, cols, CV_32S);
-
-      // step 1: initialize the bottom row
-      energy_img.row(rows - 1).copyTo(energy_map.row(rows - 1));
-
-      // step 2: move to the next row
-      for (int row = rows - 2; row >= 0; row--) {
-        // step 3: loop through all pixels in this row (cols)
-        for (int col = 0; col < cols; col++) {
-          // step 4: compute weights of adjacent pixels
-          const int &center = energy_map.at<int>(row + 1, col);
-          const int &left = (col - 1 < 0) ? std::numeric_limits<int>::max() : energy_map.at<int>(row + 1, col - 1);
-          const int &right = (col + 1 >= cols) ? std::numeric_limits<int>::max() : energy_map.at<int>(row + 1, col + 1);
-
-          // step 5: update the current pixel's weight
-          energy_map.at<int>(row, col) = energy_img.at<uchar>(row, col) + std::min({center, left, right});
-        }
-      }
-
-      return energy_map;
-    }
-
-    cv::Mat ComputeHorizontalEnergyMap(const cv::Mat &energy_img) {
-      // setup
-      int rows = energy_img.rows;
-      int cols = energy_img.cols;
-      cv::Mat energy_map = cv::Mat(rows, cols, CV_32S);
-
-      // step 1: initial the right col
-      energy_img.col(cols - 1).copyTo(energy_map.col(cols - 1));
-
-      // step 2: move to the next col
-      for (int col = cols - 2; col >= 0; col--) {
-        // step 3: loop through all pixels in this col
-        for (int row = 0; row < rows; row++) {
-          // step 4: compute weights of adjacent pixels
-          const int &center = energy_map.at<int>(row, col + 1);
-          const int &left = (row - 1 < 0) ? std::numeric_limits<int>::max() : energy_map.at<int>(col + 1, row - 1);
-          const int &right = (row + 1 >= rows) ? std::numeric_limits<int>::max() : energy_map.at<int>(col + 1, row + 1);
-
-          // step 5: update the current pixel's weight
-          energy_map.at<int>(row, col) = energy_img.at<uchar>(row, col) + std::min({center, left, right});
-        }
-      }
-
-      return energy_map;
-    }
-  }
+	// convert back to CV_8U depth and merge
+	cv::convertScaleAbs(x_nrg, x_nrg);
+	cv::convertScaleAbs(y_nrg, y_nrg);
+	cv::addWeighted(x_nrg, 0.5, y_nrg, 0.5, 0, img);
 }
+
+void ComputeVerticalEnergyMap(cv::InputArray input, cv::OutputArray output) {
+	// setup
+	cv::Mat input_mat = input.getMat();
+	int rows = input_mat.rows, cols = input_mat.cols;
+	cv::Mat res = cv::Mat(rows, cols, CV_16U);
+
+	// step 1: initialize the bottom row
+	input_mat.row(rows - 1).copyTo(res.row(rows - 1));
+
+	// step 2: move to the next row
+	for (int row = rows - 2; row >= 0; row--) {
+		// step 3: loop through all pixels in this row (cols)
+		for (int col = 0; col < cols; col++) {
+			// step 4: compute weights of adjacent pixels
+			const unsigned short &center = res.at<ushort>(row + 1, col);
+			const unsigned short &left =
+				(col - 1 < 0) ? std::numeric_limits<unsigned short>::max()
+							  : res.at<ushort>(row + 1, col - 1);
+			const unsigned short &right =
+				(col + 1 >= cols) ? std::numeric_limits<unsigned short>::max()
+								  : res.at<ushort>(row + 1, col + 1);
+
+			// step 5: update the current pixel's weight
+			res.at<ushort>(row, col) =
+				input_mat.at<uchar>(row, col) + std::min({center, left, right});
+		}
+	}
+
+	output.create(res.size(), res.type());
+	res.copyTo(output); // output depth is CV_16U
+}
+
+void ComputeHorizontalEnergyMap(cv::InputArray input, cv::OutputArray output) {
+	// setup
+	cv::Mat input_mat = input.getMat();
+	int rows = input_mat.rows, cols = input_mat.cols;
+	cv::Mat res = cv::Mat(rows, cols, CV_16U);
+
+	// step 1: initialize the right col
+	input_mat.col(cols - 1).copyTo(res.col(cols - 1));
+
+	// step 2: move to the next col
+	for (int col = cols - 2; col >= 0; col--) {
+		// step 3: loop through all pixels in this col
+		for (int row = 0; row < rows; row++) {
+			// step 4: compute weights of adjacent pixels
+			const unsigned short &center = res.at<ushort>(row, col + 1);
+			const unsigned short &below =
+				(row - 1 < 0) ? std::numeric_limits<unsigned short>::max()
+							  : res.at<ushort>(row - 1, col + 1);
+			const unsigned short &above =
+				(row + 1 >= rows) ? std::numeric_limits<unsigned short>::max()
+								  : res.at<ushort>(row + 1, col + 1);
+
+			// step 5: update the current pixel's weight
+			res.at<ushort>(row, col) = input_mat.at<uchar>(row, col) +
+									   std::min({center, below, above});
+		}
+	}
+
+	output.create(res.size(), res.type());
+	res.copyTo(output); // output depth is CV_16U
+}
+} // namespace Energy
+} // namespace SeamCarving

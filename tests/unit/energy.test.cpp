a961f0ca8c75ab530a1554b7d4d25d7bc39aec41
Andy Mina <andy.mina@mongodb.com>
2023-06-21 17:46:59 -0400

    add in nrg tests

--- tests/unit/energy.test.cpp
+++ tests/unit/energy.test.cpp
@@ -18,59 +18,59 @@ namespace sce = seam_carving::energy;
 namespace sct = seam_carving::tests;
 using sct::EnergyTest;
 
-//INSTANTIATE_TEST_SUITE_P(
-//    EnergyDataFromJSON,
-//    EnergyTest,
-//    testing::ValuesIn(
-//        sct::GetData<sct::EnergyData>(sct::kJSONDataFile)
-//    )
-//);
-//
-//TEST_P(EnergyTest, ComputeEnergy) {
-//    sct::EnergyData energy_data = GetParam();
-//
-//    cv::Mat input = energy_data.original_matrix;
-//    cv::Mat expected = energy_data.sobel_matrix;
-//    cv::Mat actual;
-//
-//    sce::ComputeEnergy(input, actual);
-//
-//    EXPECT_TRUE(sct::equalMatrices(expected, actual))
-//        << sct::PrintWithLabel(expected, "expected") << "\n"
-//        << sct::PrintWithLabel(actual, "actual");
-//}
-//
-//TEST_P(EnergyTest, ComputeVerticalMap) {
-//    sct::EnergyData energy_data = GetParam();
-//
-//    cv::Mat input = energy_data.sobel_matrix;
-//    cv::Mat expected = energy_data.vertical_map_matrix;
-//    cv::Mat actual;
-//
-//    sce::ComputeVerticalMap(input, actual);
-//
-//    EXPECT_TRUE(sct::equalMatrices(expected, actual))
-//        << sct::PrintWithLabel(expected, "expected") << "\n"
-//        << sct::PrintWithLabel(actual, "actual");
-//}
-//
-//TEST_P(EnergyTest, ComputeHorizontalMap) {
-//    sct::EnergyData energy_data = GetParam();
-//
-//    cv::Mat input = energy_data.sobel_matrix;
-//    cv::Mat expected = energy_data.horizontal_map_matrix;
-//    cv::Mat actual;
-//
-//    sce::ComputeHorizontalMap(input, actual);
-//
-//    EXPECT_TRUE(sct::equalMatrices(expected, actual))
-//        << sct::PrintWithLabel(expected, "expected") << "\n"
-//        << sct::PrintWithLabel(actual, "actual");
-//}
+INSTANTIATE_TEST_SUITE_P(
+    EnergyDataFromJSON,
+    EnergyTest,
+    testing::ValuesIn(
+        sct::GetData<sct::EnergyData>(sct::kJSONDataFile)
+    )
+);
 
-TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+TEST_P(EnergyTest, ComputeEnergy) {
+    sct::EnergyData energy_data = GetParam();
+
+    cv::Mat input = energy_data.original_matrix;
+    cv::Mat expected = energy_data.sobel_matrix;
+    cv::Mat actual;
+
+    sce::ComputeEnergy(input, actual);
+
+    EXPECT_TRUE(sct::equalMatrices(expected, actual))
+        << sct::PrintWithLabel(expected, "expected") << "\n"
+        << sct::PrintWithLabel(actual, "actual");
 }
+
+TEST_P(EnergyTest, ComputeVerticalMap) {
+    sct::EnergyData energy_data = GetParam();
+
+    cv::Mat input = energy_data.sobel_matrix;
+    cv::Mat expected = energy_data.vertical_map_matrix;
+    cv::Mat actual;
+
+    sce::ComputeVerticalMap(input, actual);
+
+    EXPECT_TRUE(sct::equalMatrices(expected, actual))
+        << sct::PrintWithLabel(expected, "expected") << "\n"
+        << sct::PrintWithLabel(actual, "actual");
+}
+
+TEST_P(EnergyTest, ComputeHorizontalMap) {
+    sct::EnergyData energy_data = GetParam();
+
+    cv::Mat input = energy_data.sobel_matrix;
+    cv::Mat expected = energy_data.horizontal_map_matrix;
+    cv::Mat actual;
+
+    sce::ComputeHorizontalMap(input, actual);
+
+    EXPECT_TRUE(sct::equalMatrices(expected, actual))
+        << sct::PrintWithLabel(expected, "expected") << "\n"
+        << sct::PrintWithLabel(actual, "actual");
+}
+
+//TEST(HelloTest, BasicAssertions) {
+//    // Expect two strings not to be equal.
+//    EXPECT_STRNE("hello", "world");
+//    // Expect equality.
+//    EXPECT_EQ(7 * 6, 42);
+//}

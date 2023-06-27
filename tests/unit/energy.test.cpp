dcf86aeac89b033076d4a9eecf90194dc5fdeb6f
Andy Mina <andy.mina@mongodb.com>
2023-06-26 21:37:26 -0400

    just one test

--- tests/unit/energy.test.cpp
+++ tests/unit/energy.test.cpp
@@ -46,30 +46,30 @@ TEST_P(EnergyTest, ComputeEnergy) {
         << sct::PrintWithLabel(actual, "actual");
 }
 
-TEST_P(EnergyTest, ComputeVerticalMap) {
-    sct::EnergyData energy_data = GetParam();
-
-    cv::Mat input = energy_data.sobel_matrix;
-    cv::Mat expected = energy_data.vertical_map_matrix;
-    cv::Mat actual;
-
-    sce::ComputeVerticalMap(input, actual);
-
-    EXPECT_TRUE(sct::equalMatrices(expected, actual))
-        << sct::PrintWithLabel(expected, "expected") << "\n"
-        << sct::PrintWithLabel(actual, "actual");
-}
-
-TEST_P(EnergyTest, ComputeHorizontalMap) {
-    sct::EnergyData energy_data = GetParam();
-
-    cv::Mat input = energy_data.sobel_matrix;
-    cv::Mat expected = energy_data.horizontal_map_matrix;
-    cv::Mat actual;
-
-    sce::ComputeHorizontalMap(input, actual);
-
-    EXPECT_TRUE(sct::equalMatrices(expected, actual))
-        << sct::PrintWithLabel(expected, "expected") << "\n"
-        << sct::PrintWithLabel(actual, "actual");
-}
+//TEST_P(EnergyTest, ComputeVerticalMap) {
+//    sct::EnergyData energy_data = GetParam();
+//
+//    cv::Mat input = energy_data.sobel_matrix;
+//    cv::Mat expected = energy_data.vertical_map_matrix;
+//    cv::Mat actual;
+//
+//    sce::ComputeVerticalMap(input, actual);
+//
+//    EXPECT_TRUE(sct::equalMatrices(expected, actual))
+//        << sct::PrintWithLabel(expected, "expected") << "\n"
+//        << sct::PrintWithLabel(actual, "actual");
+//}
+//
+//TEST_P(EnergyTest, ComputeHorizontalMap) {
+//    sct::EnergyData energy_data = GetParam();
+//
+//    cv::Mat input = energy_data.sobel_matrix;
+//    cv::Mat expected = energy_data.horizontal_map_matrix;
+//    cv::Mat actual;
+//
+//    sce::ComputeHorizontalMap(input, actual);
+//
+//    EXPECT_TRUE(sct::equalMatrices(expected, actual))
+//        << sct::PrintWithLabel(expected, "expected") << "\n"
+//        << sct::PrintWithLabel(actual, "actual");
+//}

9bd3133502873c24b672b1893587efd0b131abdf
Andy Mina <mina.andy54@gmail.com>
2023-11-04 11:31:11 -0400

    add convenience methods for multiple seams

--- libseam_carving/seam_carving/carver.hpp
+++ libseam_carving/seam_carving/carver.hpp
@@ -15,13 +15,14 @@
 // std
 #include <algorithm>
 #include <limits>
+#include <vector>
 
 namespace seam_carving {
     /**
      * Base class for all carvers
      */
     class Carver {
-    public:
+    protected:
         /**
          * @param img the image to find the optimal seam in
          * @returns the optimal seam to be removed
@@ -30,9 +31,9 @@ namespace seam_carving {
         static Seam FindHorizontalSeam(cv::InputArray img);
 
         /**
-         * @param seam the seam to be removed
-         * @param img the target image
-         */
+ * @param seam the seam to be removed
+ * @param img the target image
+ */
         static void RemoveVerticalSeam(const Seam &seam, cv::InputArray input, cv::OutputArray output);
         static void RemoveHorizontalSeam(const Seam &seam, cv::InputArray input, cv::OutputArray output);
 
@@ -42,6 +43,49 @@ namespace seam_carving {
          */
         static void InsertVerticalSeam(const Seam &seam, cv::InputArray input, cv::OutputArray output);
         static void InsertHorizontalSeam(const Seam &seam, cv::InputArray input, cv::OutputArray output);
+
+    public:
+        /**
+         * Finds the first k vertical/horizontal seams for removal.
+         *
+         * @param k the number of seams to find
+         * @param input the image to find the seams in
+         * @return a vector of the first k seams for removal
+         */
+        static std::vector<Seam> FindVerticalSeams(const int& k, cv::InputArray input);
+        static std::vector<Seam> FindHorizontalSeams(const int& k, cv::InputArray input);
+
+        /**
+         * Inserts k vertical/horizontal seams.
+         * @param k the number of seams to insert
+         * @param input the image to find seams in
+         * @param output the image to insert seams into
+         */
+        static void InsertVerticalSeams(const int& k, cv::InputArray input, cv::OutputArray output);
+        static void InsertHorizontalSeams(const int& k, cv::InputArray input, cv::OutputArray output);
+
+        /**
+         * Inserts the specified seams, regardless of direction, into the image appropriately.
+         * @param seams the seams to be inserted
+         * @param output the image to insert seams into
+         */
+        static void InsertSeams(const std::vector<Seam>& seams, cv::InputOutputArray output);
+
+        /**
+         * Removes k vertical/horizontal seams.
+         * @param k the number of seams to remove
+         * @param input the image to find seams in
+         * @param output the image to remove seams from
+         */
+        static void RemoveVerticalSeams(const int& k, cv::InputArray input, cv::OutputArray output);
+        static void RemoveHorizontalSeams(const int& k, cv::InputArray input, cv::OutputArray output);
+
+        /**
+         * Removes the specified seams, regardless of direction, from the image appropriately.
+         * @param seams the seams to be removed
+         * @param output the image to remove seams from
+         */
+        static void RemoveSeams(const std::vector<Seam>& seams, cv::InputOutputArray output);
 
         /**
          * @param rows the target number of rows for the new image
